b5acf24097fce4bd0ebc6fe5e4327567120049ce
kefaming <kefaming@163.com>
2021-11-17 01:01:57 +0800

    Initial commit

---
+++ src/api/index.js
@@ -0,0 +1,35 @@
+import request from '../utils/request';
+import service from "../utils/request";
+
+export const fetchData = query => {
+    return request({
+        // url: './table.json',
+        url: '/selectList',
+        method: 'get',
+        params: query
+    });
+};
+
+export const removeData = query => {
+    return request({
+        url: '/removeById',
+        method: 'get',
+        params: query
+    });
+};
+
+
+export const modifyData = user => {
+    // return request({
+    //     // url: './table.json',
+    //     url: '/editById',
+    //     method: 'put',
+    //     params: user
+    // });
+
+    return request({
+        method: 'post',
+        url: 'editById',
+        data: user
+    });
+};
